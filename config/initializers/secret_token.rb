a3ab392eb9df9ecb28edeca058bc409aa212e6bf
Bryan Mezue <bryan.mezue@gmail.com>
2013-04-18 02:51:17 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lulurails::Application.config.secret_token = '34ea74166699a4ff7c7985c84f7482688c2c5196a1c38dbeb1670dff08af959b99edd4671034b3a681beb1244bf56b1a60aba2801224acc7a84b4dab4bbc9a59'
